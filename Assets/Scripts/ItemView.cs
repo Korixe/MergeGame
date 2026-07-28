f8571316ed1c4685b21d02ad2190ece97610fd19
Matiash Anastasia <142342503+Korixe@users.noreply.github.com>
2026-07-28 17:11:24 +0300

    feat: add drag logic

--- Assets/Scripts/ItemView.cs
+++ Assets/Scripts/ItemView.cs
@@ -1,20 +1,52 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ItemView : MonoBehaviour
+public class ItemView : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public ItemData itemData;
     private Image _image;
+    private Vector2 _originalPosition;
+    private RectTransform _rectTransform;
+    private CanvasGroup _canvasGroup;
+    private Transform _originalParentCell;
+
+
 
     private void Awake()
     {
         _image = GetComponent<Image>();
+        _rectTransform = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+
+        if (_canvasGroup == null)
+            _canvasGroup = gameObject.AddComponent<CanvasGroup>();
     }
 
     public void SetItemData(ItemData data)
     {
         itemData = data;
         _image.sprite = itemData.sprite;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _originalParentCell = transform.parent;
+        _originalPosition = _rectTransform.anchoredPosition;
+        _canvasGroup.blocksRaycasts = false;
+
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _rectTransform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _canvasGroup.blocksRaycasts = true;
     }
 
 }
