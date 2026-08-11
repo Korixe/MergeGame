c32a36ba2f720e3be7e62afd323cbfcdff0c5270
Matiash Anastasia <142342503+Korixe@users.noreply.github.com>
2026-08-11 17:51:44 +0300

    feat: add item id and itemDatabase

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ItemData", menuName = "Scriptable Objects/ItemData")]
 public class ItemData : ScriptableObject
 {
+    public string itemID;
     public string itemName;
     public Sprite sprite;
     public int type;
