90b4f4785c3aa41cb69bfbe8876fb7bcb7dd89c5
Matiash Anastasia <142342503+Korixe@users.noreply.github.com>
2026-08-16 18:14:08 +0300

    feat: add order view

--- Assets/Scripts/Orders/OrderView.cs
+++ Assets/Scripts/Orders/OrderView.cs
@@ -1,7 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class OrderView : MonoBehaviour
 {
-    public OrderData orderData;
-    
+    public Button completeOrderButton;
+    public TextMeshProUGUI orderNameText;
+    public TextMeshProUGUI orderValueText;
+
+    private ActiveOrder _activeOrder;
+
+    public void SetActiveOrder(ActiveOrder activeOrder)
+    {
+        _activeOrder = activeOrder;
+        orderNameText.text = _activeOrder.orderData.orderName;
+        orderValueText.text = $"Value: {_activeOrder.orderData.orderValue}";\
+
+        completeOrderButton.onClick.RemoveAllListeners();
+        completeOrderButton.onClick.AddListener(OnCompleteOrderButtonClicked);
+    }
+
+    private void OnCompleteOrderButtonClicked()
+    {
+        bool orderCompleted = OrderManager.Instance.CompleteOrder(_activeOrder);
+        if (orderCompleted)
+        {
+            Debug.Log($"Order {_activeOrder.orderData.orderName} completed!");
+            OrderManager.Instance.activeOrders.Remove(_activeOrder);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log($"Order {_activeOrder.orderData.orderName} could not be completed");
+        }
+    }
+
 }
