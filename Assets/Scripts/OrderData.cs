f1f86777cdce7ab1bd31169722ea2cece1f40f98
Matiash Anastasia <142342503+Korixe@users.noreply.github.com>
2026-08-16 17:36:12 +0300

    feat: add order data scriptable obj

---
+++ Assets/Scripts/OrderData.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "OrderData", menuName = "Scriptable Objects/OrderData")]
+public class OrderData : ScriptableObject
+{
+    public string orderID;
+    public string orderName;
+    public int orderValue;
+    public List<ItemData> requiredItems;
+    
+}
