59c74e730416675c87c7c8be4e5b92c73817b25d
Matiash Anastasia <142342503+Korixe@users.noreply.github.com>
2026-08-24 17:40:22 +0300

    feat: add energy on canvas and regeneration

--- Assets/Scripts/EnergyManager.cs
+++ Assets/Scripts/EnergyManager.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 [System.Serializable]
 public class EnergyManager : MonoBehaviour
@@ -9,20 +10,56 @@ public class EnergyManager : MonoBehaviour
     public TextMeshProUGUI energyText;
     private int _energyAmount;
     private int _maxEnergy = 100;
-    private float _energyRegenTime = 300f; 
+    private float _energyRegenTime = 1f; // 300 seconds
+    private float _currentRegenerationTime;
+    private long _lastSyncTime;
     public int energyAmount => _energyAmount;
+    public long lastSyncTime => _lastSyncTime;
 
     private void Awake()
     {
         Instance = this;
-        UpdateEnergyText();
+    }
+
+    private void Start()
+    {
+        _currentRegenerationTime = _energyRegenTime;
+    }
+
+    private void Update()
+    {
+        if (_energyAmount < _maxEnergy)
+        {
+            _currentRegenerationTime -= Time.deltaTime;
+
+            if (_currentRegenerationTime <= 0f)
+            {
+                RegenerateEnergy();
+                _currentRegenerationTime = _energyRegenTime;
+            }
+        }
+    }
+
+    public void SyncTime()
+    {
+        _lastSyncTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
     }
 
     public void SetEnergy(int amount)
     {
         _energyAmount = amount;
         UpdateEnergyText();
-    }  
+    }
+
+    public void RegenerateEnergy()
+    {
+        if (_energyAmount < _maxEnergy)
+        {
+            _energyAmount++;
+            UpdateEnergyText();
+            SyncTime();
+        }
+    } 
 
     public void AddEnergy(int amount)
     {
@@ -37,6 +74,13 @@ public class EnergyManager : MonoBehaviour
 
         _energyAmount -= amount;
         UpdateEnergyText();
+
+        if (_energyAmount == _maxEnergy - amount)
+        {
+            _currentRegenerationTime = _energyRegenTime;
+            SyncTime();
+        }
+        
         return true;
     }
 
