35ba9acd446fbf34604b8f43829bcfa0dbabfbac
Matiash Anastasia <142342503+Korixe@users.noreply.github.com>
2026-08-25 18:06:27 +0300

    feat: add energy and currency text, add play button

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
-public class MainMenuManager
+public class MainMenuManager : MonoBehaviour
 {
-    
+    public TextMeshProUGUI currencyText;
+    public TextMeshProUGUI energyText;
+    public string gameSceneName = "GameScene";
+
+    private void Start()
+    {
+        DisplayCurrency();
+        DisplayEnergy();
+    }
+
+    private void DisplayCurrency()
+    {
+
+    }
+    private void DisplayEnergy()
+    {
+
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
 }
