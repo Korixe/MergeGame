cad3024dc8ba98d487447c60c09dc2099527be65
Matiash Anastasia <142342503+Korixe@users.noreply.github.com>
2026-08-28 22:14:59 +0300

    feat: add energy display to main menu

---
+++ Assets/Scripts/EnergyDisplay.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+public class EnergyDisplay : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+
+    private void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void OnEnable()
+    {
+        EnergyManager.Instance.OnEnergyChanged += UpdateText;
+        UpdateText(EnergyManager.Instance.energyAmount);
+    }
+
+    private void OnDisable()
+    {
+        if (EnergyManager.Instance != null)
+            EnergyManager.Instance.OnEnergyChanged -= UpdateText;
+    }
+
+    private void UpdateText(int amount)
+    {
+        _text.text = amount.ToString();
+    }
+}
