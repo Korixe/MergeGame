a451be0a8ca9d17ef2023d1cd7594c44f8ad48fd
Matiash Anastasia <142342503+Korixe@users.noreply.github.com>
2026-08-28 22:22:18 +0300

    refactor: remove text from main menu manager

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,33 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public TextMeshProUGUI currencyText;
-    public TextMeshProUGUI energyText;
     public string gameSceneName = "GameScene";
-
-    private void Start()
-    {
-        UpdateEnergyText(EnergyManager.Instance.energyAmount);
-        EnergyManager.Instance.OnEnergyChanged += UpdateEnergyText;
-        DisplayEnergy();
-    }
-
-    private void UpdateEnergyText(int newEnergyAmount)
-    {
-        energyText.text = newEnergyAmount.ToString();
-    }
-    private void DisplayEnergy()
-    {
-
-    }
 
     public void OnPlayButtonClicked()
     {
         SceneManager.LoadScene(gameSceneName);
-    }
-    private void OnDestroy()
-    {  
-        if (EnergyManager.Instance != null)
-            EnergyManager.Instance.OnEnergyChanged -= UpdateEnergyText;
     }
 }
